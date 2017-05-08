032c8b1727f90f1865ac66619088e2cd4b25f16b
Jimmy Wu <wujimmy94@gmail.com>
2017-05-08 17:06:19 -0400

    merge conflicts fix

--- Note_Taker/config/routes.rb
+++ Note_Taker/config/routes.rb
@@ -1,10 +1,7 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
   resources :notes
   resources :classrooms
-=======
   resources :professors
   resources :profiles
->>>>>>> cbe9db176f0a68e4d1fe0a1d8d111af04f8663c6
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- Note_Taker/db/schema.rb
+++ Note_Taker/db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
 ActiveRecord::Schema.define(version: 20170508205256) do
 
   create_table "classrooms", force: :cascade do |t|
@@ -25,8 +24,7 @@ ActiveRecord::Schema.define(version: 20170508205256) do
   create_table "notes", force: :cascade do |t|
     t.string   "content"
     t.string   "title"
-=======
-ActiveRecord::Schema.define(version: 20170508205252) do
+  end
 
   create_table "professors", force: :cascade do |t|
     t.string   "fname"
@@ -45,7 +43,6 @@ ActiveRecord::Schema.define(version: 20170508205252) do
     t.string   "school"
     t.string   "classroom"
     t.string   "study"
->>>>>>> cbe9db176f0a68e4d1fe0a1d8d111af04f8663c6
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
