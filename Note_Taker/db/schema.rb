48550c3eeb3dea0eeeb4e2570bc53660bb09aeae
Jimmy Wu <wujimmy94@gmail.com>
2017-05-11 17:55:42 -0400

    user model fix

--- Note_Taker/db/schema.rb
+++ Note_Taker/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170511194604) do
+ActiveRecord::Schema.define(version: 20170511215239) do
 
   create_table "classrooms", force: :cascade do |t|
     t.string   "name"
@@ -70,7 +70,6 @@ ActiveRecord::Schema.define(version: 20170511194604) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
-    t.integer  "profile_id"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
